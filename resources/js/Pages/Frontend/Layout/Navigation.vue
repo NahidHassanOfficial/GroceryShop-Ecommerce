f46333f8f9beff6ca37232b2447c97f5e783f12b
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2024-11-02 22:58:41 +0600

    cart count added & navigation for responsive layout updated

--- resources/js/Pages/Frontend/Layout/Navigation.vue
+++ resources/js/Pages/Frontend/Layout/Navigation.vue
@@ -127,6 +127,23 @@ import { cartList, removeCartItem } from '../Components/Utils/CartWishManage';
                                 <div class="list-inline me-2">
                                     <div class="list-inline-item">
 
+                                        <Link :href="route('profile.wish-list')" class="text-muted position-relative">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="feather feather-heart">
+                                            <path
+                                                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
+                                            </path>
+                                        </svg>
+                                        <span
+                                            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
+                                            {{ $page.props.wishlistCount }}
+                                        </span>
+                                        </Link>
+                                    </div>
+                                    <div class="list-inline-item">
+
                                         <Link v-if="$page.props.isAuth" :href="route('profile')" class="text-muted">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -147,7 +164,7 @@ import { cartList, removeCartItem } from '../Components/Utils/CartWishManage';
                                     </div>
                                     <div class="list-inline-item">
 
-                                        <a class="text-muted" data-bs-toggle="offcanvas"
+                                        <a class="text-muted  position-relative" data-bs-toggle="offcanvas"
                                             data-bs-target="#offcanvasRight" href="#offcanvasExample" role="button"
                                             aria-controls="offcanvasRight">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
@@ -159,6 +176,11 @@ import { cartList, removeCartItem } from '../Components/Utils/CartWishManage';
                                                 </line>
                                                 <path d="M16 10a4 4 0 0 1-8 0"></path>
                                             </svg>
+                                            <span
+                                                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
+                                                {{ cartList.length }}
+                                                <span class="visually-hidden">unread messages</span>
+                                            </span>
                                         </a>
                                     </div>
 
@@ -256,7 +278,7 @@ import { cartList, removeCartItem } from '../Components/Utils/CartWishManage';
                                     </svg>
                                     <span
                                         class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
-                                        1
+                                        {{ cartList.length }}
                                         <span class="visually-hidden">unread messages</span>
                                     </span>
                                 </a>
